963f31a93346ba963a222ab055948913cfed2d4b
Nagaraj Botekar <nagaraj81471@gmail.com>
2026-02-22 19:59:19 +0530

    Backend url setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import Search from './pages/Search'
 import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
-export const serverUrl="http://localhost:8000"
+export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:8000"
 function App() {
    getCurrentUser()
    getSuggestedUsers()
